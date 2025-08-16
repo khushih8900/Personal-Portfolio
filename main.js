af3be4ba808da2abc40d40f4e0479e280ba15469
khushih8900 <khushih191@gmail.com>
2025-08-16 20:57:56 +0600

    add services and resume section

--- main.js
+++ main.js
@@ -1,12 +1,12 @@
 const list = document.querySelectorAll('.list ');
 
-function activeLink() {
-  list.forEach((item) => item.classList.remove('active'));
-  this.classList.add('active');
+function activeMenuLink() {
+  list.forEach((item) => item.classList.remove('active-menu'));
+  this.classList.add('active-menu');
 }
 
-list.forEach((item) => item.addEventListener('click', activeLink));
-/menu-icon/ 
+list.forEach((item) => item.addEventListener('click', activeMenuLink));
+/menu-icon/
 
 const menuIcon = document.getElementById('menuIcon');
 const sideBar = document.getElementById('sideBar');
@@ -14,4 +14,22 @@ const sideBar = document.getElementById('sideBar');
 menuIcon.addEventListener('click', function () {
   sideBar.classList.toggle('active');
 });
-/ home nav bar/ 
+/ resume button/
+const resumeBtn = document.querySelectorAll('.resume-btn');
+const details = document.querySelectorAll('.details'); // <-- সব details ধরো
+
+resumeBtn.forEach((btn, index) => {
+  btn.addEventListener('click', function () {
+    
+    resumeBtn.forEach((b) => b.classList.remove('actives-resume'));
+    
+    details.forEach((d) => d.classList.remove('active-details'));
+
+  
+    this.classList.add('actives-resume');
+    
+    details[index].classList.add('active-details');
+  });
+});
+
+
