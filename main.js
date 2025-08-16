27e6012a69e54714bfce68c259a700f369f8843f
khushih8900 <khushih191@gmail.com>
2025-08-17 01:04:16 +0600

    add portfolio section

--- main.js
+++ main.js
@@ -20,16 +20,43 @@ const details = document.querySelectorAll('.details'); // <-- সব details ধ
 
 resumeBtn.forEach((btn, index) => {
   btn.addEventListener('click', function () {
-    
+
     resumeBtn.forEach((b) => b.classList.remove('actives-resume'));
-    
+
     details.forEach((d) => d.classList.remove('active-details'));
 
-  
+
     this.classList.add('actives-resume');
-    
+
     details[index].classList.add('active-details');
   });
+});
+/ slider section/ 
+const arrowRight = document.querySelector('.nav-btn .right');
+const arrowLeft = document.querySelector('.nav-btn .leftBtn');
+const imgSlide = document.querySelector('.portfolio-item');
+const totalSlides = document.querySelectorAll('.portfolio-item .portfolio-img').length;
+
+let index = 0;
+
+const activePortfolio = () => {
+  imgSlide.style.transform = `translateX(calc(${index * -100}% - ${index * 2}rem))`;
+};
+
+arrowRight.addEventListener('click', () => {
+  if (index < totalSlides - 1) {
+    index++;
+    arrowRight.classList.add('disable')
+  }
+  activePortfolio();
+});
+
+arrowLeft.addEventListener('click', () => {
+  if (index > 0) {
+    index--;
+    arrowLeft.classList.add('disable');
+  }
+  activePortfolio();
 });
 
 
